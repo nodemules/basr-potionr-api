ef23f4f4997ed62be0e7a1c5d3321ac427ca1956
brenthaertlein <brenthaertlein@gmail.com>
2017-12-14 19:55:43 -0500

    initial commit

---
+++ src/main/java/com/nodemules/api/potion/BasrPotionApplication.java
@@ -0,0 +1,12 @@
+package com.nodemules.api.potion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasrPotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasrPotionApplication.class, args);
+	}
+}
